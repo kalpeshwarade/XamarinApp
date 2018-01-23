de6e7cc1673488cfe7f44995e1cf0ee895b845d8
kalpesh warade <warade.kalpesh4444@gmail.com>
2018-01-23 15:15:24 +0530

    initital commit

---
+++ WeatherApp1/WeatherApp1/Core.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class Core
+    {
+        public static async Task<Weather> GetWeather(string zipCode)
+        {
+            //Sign up for a free API key at http://openweathermap.org/appid
+            string key = "200384880dd803fea8141c5e73ace40b";
+            string queryString = "http://api.openweathermap.org/data/2.5/weather?zip="
+                + zipCode + ",&appid=" + key;
+
+            //Make sure developers running this sample replaced the API key
+            if (key == "200384880dd803fea8141c5e73ace40b")
+            {
+                throw new ArgumentException(message: "You must obtain an API key from openweathermap.org/appid and save it in the 'key' variable");
+            }
+
+            var results = await DataService.getDataFromService(queryString).ConfigureAwait(false);
+
+            if (results["weather"] != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = (string)results["name"];
+                weather.Temperature = (string)results["main"]["temp"] + " F";
+                weather.Wind = (string)results["wind"]["speed"] + " mph";
+                weather.Humidity = (string)results["main"]["humidity"] + " %";
+                weather.Visibility = (string)results["weather"][0]["main"];
+
+                DateTime time = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+                DateTime sunrise = time.AddSeconds((double)results["sys"]["sunrise"]);
+                DateTime sunset = time.AddSeconds((double)results["sys"]["sunset"]);
+                weather.Sunrise = sunrise.ToString() + " UTC";
+                weather.Sunset = sunset.ToString() + " UTC";
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
